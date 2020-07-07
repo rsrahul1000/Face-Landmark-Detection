ae7091bf91e89e522c2918d6843cca900d20ab6c
Rahul Sharma <rsrahul1000@gmail.com>
2020-07-07 16:28:54 +0530

    error: trying to add hat on top of image

--- focal_point_Detection_video.py
+++ focal_point_Detection_video.py
@@ -1,14 +1,18 @@
 import cv2
 import numpy as np
 import dlib
+from PIL import Image, ImageDraw, ImageFilter
 
 detector = dlib.get_frontal_face_detector()
 
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 cap = cv2.VideoCapture(0)
-
+hat = Image.open('graduation_hat.png') #cv2.imread('graduation_hat.png')
+# Set initial value of weights
+alpha = 0.4
 while True:
 	_, frame = cap.read()
+	print(type(frame))
 	gray = cv2.cvtColor(src=frame, code=cv2.COLOR_BGR2GRAY)
 	faces = detector(gray)
 
@@ -22,7 +26,11 @@ while True:
 		for n in range(0,68):
 			x = landmarks.part(n).x
 			y = landmarks.part(n).y
-			cv2.circle(img=frame, center=(x,y), radius=1, color=(0,255,0), thickness=-2)
+			#frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+			frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+			frame = Image.fromarray(frame)
+			frame.paste(np.asarray(hat), (x,y))
+			#cv2.circle(img=frame, center=(x,y), radius=1, color=(0,255,0), thickness=-2)
 
 		cv2.imshow(winname="Face", mat=frame)
 	if cv2.waitKey(delay=1) == 27:
